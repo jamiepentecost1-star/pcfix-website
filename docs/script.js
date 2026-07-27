d2ae0809f8c7146938280a18cd352d6ecc708133
jamiepentecost1-star <jamie.pentecost1@gmail.com>
2026-07-26 23:01:46 -0500

    Delete docs directory

--- docs/script.js
+++
@@ -1,1 +0,0 @@
-const c=document.getElementById('matrix'),x=c.getContext('2d');function r(){c.width=innerWidth;c.height=innerHeight}r();onresize=r;const s=18,l='01ABCDEFGHIJKLMNOPQRSTUVWXYZ';let cols=Math.floor(in[...]
